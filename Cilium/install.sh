23b92a1e17eaa90f17d9509926ec4ad80540f60b
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-22 16:17:27 +0200

    Create install.sh

---
+++ Cilium/install.sh
@@ -0,0 +1,14 @@
+CLI_ARCH=amd64
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-${CLI_ARCH}.tar.gz
+tar xzvf cilium-linux-${CLI_ARCH}.tar.gz -C /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz
+
+cilium version --client
+
+cilium install \
+  --set ipam.mode=cluster-pool \
+  --set ipam.operator.clusterPoolIPv4PodCIDRList="{192.168.0.0/16}" \
+  --set kubeProxyReplacement=false
+
+cilium hubble ui
+cilium hubble enable --ui
