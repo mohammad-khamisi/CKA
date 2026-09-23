5e67362edce93c3037911b9670edff3c35c4a994
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-23 16:01:44 +0200

    Update install.sh

--- Longhorn/install.sh
+++ Longhorn/install.sh
@@ -1,4 +1,24 @@
+# both master & worker
 apt update
 apt install -y open-iscsi nfs-common
 systemctl enable --now iscsid
 systemctl status iscsid --no-pager
+
+# master
+# نصب helm اگر ندارید
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# افزودن ریپازیتوری رسمی Longhorn
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+
+# نصب با تنظیم replica پیش‌فرض روی 2 (متناسب با کلاستر دونودی شما)
+helm install longhorn longhorn/longhorn \
+  --namespace longhorn-system \
+  --create-namespace \
+  --set defaultSettings.defaultReplicaCount=2
+
+kubectl get pods -n longhorn-system -w
+
+kubectl get nodes.longhorn.io -n longhorn-system
+kubectl get storageclass
