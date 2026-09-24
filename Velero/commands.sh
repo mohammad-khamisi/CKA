62416cdc3d16c35e315e95d54b24c8814d36621d
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-24 15:07:18 +0200

    Update commands.sh

--- Velero/commands.sh
+++ Velero/commands.sh
@@ -5,4 +5,8 @@ velero backup create first-test-backup --include-namespaces default
 velero backup describe first-test-backup --details
 velero backup logs first-test-backup
 
+kubectl delete pods --all -n default
+kubectl delete pvc --all -n default
+kubectl delete pv restored-test-vol --ignore-not-found
 
+velero restore create first-test-restore --from-backup first-test-backup
