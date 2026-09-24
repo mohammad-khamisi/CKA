679af5d3680e65077a236feb677f539c0239dbc7
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-24 15:57:25 +0200

    Update install.sh

--- Rancher/install.sh
+++ Rancher/install.sh
@@ -11,3 +11,33 @@ helm install cert-manager jetstack/cert-manager \
   --set cainjector.replicaCount=1
 
 kubectl get pods -n cert-manager -w
+
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo update
+
+kubectl create namespace cattle-system
+
+helm install rancher rancher-latest/rancher \
+  --namespace cattle-system \
+  --set hostname=rancher.172.28.100.203.nip.io \
+  --set bootstrapPassword=ChangeMe123! \
+  --set replicas=1 \
+  --set ingress.enabled=false
+
+kubectl get pods -n cattle-system -w
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
