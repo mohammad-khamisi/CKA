73ebd3f0f762abe511a3d29e06df5338b62542b9
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-24 15:52:04 +0200

    Update install.sh

--- Rancher/install.sh
+++ Rancher/install.sh
@@ -1,1 +1,13 @@
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
 
+kubectl create namespace cert-manager
+
+helm install cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --set crds.enabled=true \
+  --set replicaCount=1 \
+  --set webhook.replicaCount=1 \
+  --set cainjector.replicaCount=1
+
+kubectl get pods -n cert-manager -w
