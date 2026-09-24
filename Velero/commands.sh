da7e5f8a2fce04fb29dfa617a0535309d8f25aea
mohammad-khamisi <31432794+mohammad-khamisi@users.noreply.github.com>
2026-09-24 15:30:38 +0200

    Update commands.sh

--- Velero/commands.sh
+++ Velero/commands.sh
@@ -10,3 +10,29 @@ kubectl delete pvc --all -n default
 kubectl delete pv restored-test-vol --ignore-not-found
 
 velero restore create first-test-restore --from-backup first-test-backup
+
+velero backup get
+
+--------------------------------------------
+# هر روز ساعت ۲ بامداد، از namespace پیش‌فرض، با نگه‌داری ۷ روزه
+velero schedule create daily-default-backup \
+  --schedule="0 2 * * *" \
+  --include-namespaces default \
+  --ttl 168h0m0s
+
+# هر ساعت (برای تست سریع‌تر)
+velero schedule create hourly-test \
+  --schedule="@every 1h" \
+  --include-namespaces default \
+  --ttl 24h0m0s
+
+# مثال ترکیبی
+velero backup create app-only-backup \
+  --include-namespaces default \
+  --selector app=nginx \
+  --ttl 72h0m0s
+
+
+
+
+
